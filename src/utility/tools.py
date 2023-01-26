04fdb2a1e24e16b37125e54e5d30315fa400b741
RobinShannon <shannon.rj@googlemail.com>
2023-01-26 22:56:16 +0000

    MechanismGeneration reintoduction first pass

--- src/utility/tools.py
+++ src/utility/tools.py
@@ -108,11 +108,11 @@ def Fourier2D(coeffs, angles,number_of_c):
     pot = 0.0
     pot += coeffs[0][0] / 4.0
     for i in range(1,number_of_c) :
-        pot += (coeffs[0][i] * np.cos(i * angles[0]) + coeffs[1][i] * np.sin(i) * angles[0]) / 2.0
+        pot += (coeffs[0][i] * np.cos(i * angles[0]) + coeffs[1][i] * np.sin(i) * angles[1]) / 2.0
 
     for i in range(1,number_of_c):
         oneDIndex = i * number_of_c
-        pot += (coeffs[0][oneDIndex] * np.cos(i * angles[1]) + coeffs[2][oneDIndex] * np.sin(i * angles[1])) / 2.0
+        pot += (coeffs[0][oneDIndex] * np.cos(i * angles[0]) + coeffs[2][oneDIndex] * np.sin(i * angles[1])) / 2.0
 
     for i in range(1, number_of_c):
         for j in range(1,number_of_c):
