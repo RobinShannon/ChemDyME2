0bddf29a40dc3ec628ba5c57ded3b37d4ac895ab
RobinShannon <shannon.rj@googlemail.com>
2023-01-26 15:07:21 +0000

    MechanismGeneration reintoduction first pass

--- src/mechanism_generation/mol_types/species.py
+++ src/mechanism_generation/mol_types/species.py
@@ -458,7 +458,7 @@ class species:
                 a.append(np.radians(hmol.get_dihedral(int(dihedrals[1][0]) - 1, int(dihedrals[1][1]) - 1, int(dihedrals[1][2]) - 1,int(dihedrals[1][3]) - 1)))
                 angle_arr_1D.append(a)
             rot_array_2D.append(arr)
-        coeffs=tl.fitFourier2Dsimp(ene_arr_1D, angle_arr_1D, 5)
+        coeffs=tl.fitFourier2Dsimp(ene_arr_1D, angle_arr_1D,15)
         check = []
         for a,e in zip(angle_arr_1D,ene_arr_1D):
             fit_ene = tl.Fourier2D(coeffs,a, 5)
