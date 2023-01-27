5684780fa7343786a1cf52052280adb6e94bbe99
RobinShannon <shannon.rj@googlemail.com>
2023-01-27 10:22:20 +0000

    MechanismGeneration reintoduction first pass

--- src/utility/tools.py
+++ src/utility/tools.py
@@ -96,10 +96,10 @@ def fitFourier2Dsimp(energies,angles,coeffs):
                 c2 += e * np.cos(i * a[0]) * np.sin(j * a[1]) * w
                 c3 += e * np.sin(i * a[0]) * np.cos(j * a[1]) * w
                 c4 += e * np.sin(i * a[0]) * np.sin(j * a[1]) * w
-            c1 *= 4 / ( 9 * len(energies))
-            c2 *= 4 / (9 * len(energies))
-            c3 *= 4 / (9 * len(energies))
-            c4 *= 4 / (9 * len(energies))
+            c1 *= 4 / ( 9 * (len(energies)+np.sqrt(len(energies))))
+            c2 *= 4 / ( 9 * (len(energies)+np.sqrt(len(energies))))
+            c3 *= 4 / ( 9 * (len(energies)+np.sqrt(len(energies))))
+            c4 *= 4 / ( 9 * (len(energies)+np.sqrt(len(energies))))
             c11.append(c1)
             c22.append(c2)
             c33.append(c3)
