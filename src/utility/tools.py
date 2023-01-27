c97b75402b7923d68f68c4cad9fe4d0a25c71caf
RobinShannon <shannon.rj@googlemail.com>
2023-01-27 09:23:25 +0000

    MechanismGeneration reintoduction first pass

--- src/utility/tools.py
+++ src/utility/tools.py
@@ -69,12 +69,17 @@ def fitFourier2Dsimp(energies,angles,coeffs):
     c33 = []
     c44 = []
     size = int(np.sqrt(len(energies)))
-    W1_1 = [2]*size
-    W1_1[1::2] = [4]*int(size/2)
+    W1 = [2]*size
+    W1[1:-2:2] = [4] * int(size/2)-1
+    W1[2:-2:1] = [8] * int(size/2)
+    W2 = [i * 2 for i in W1]
     W = []
     for i in range(0,size):
         for j in range(0,size):
-            W.append(W1_1[j])
+            if i % 2 == 0:
+                W.append[W1[j]]
+            else:
+                W.append(W2[j])
 
 
     print(str(W))
