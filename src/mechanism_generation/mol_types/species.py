cd350535adf6d675c5e6e6d060eed0753f4095e4
RobinShannon <shannon.rj@googlemail.com>
2023-01-29 16:43:14 +0000

    MechanismGeneration reintoduction first pass

--- src/mechanism_generation/mol_types/species.py
+++ src/mechanism_generation/mol_types/species.py
@@ -493,7 +493,7 @@ class species:
         for a,e in zip(angle_arr_1D,ene_arr_1D):
             fit_ene = tl.Fourier2Dalt(coeffs,a, f_coeffs)
             try:
-                chi += abs(fit_ene - e) / ene
+                chi += abs(fit_ene - e) / e
             except:
                 pass
             check.append([fit_ene,e])
