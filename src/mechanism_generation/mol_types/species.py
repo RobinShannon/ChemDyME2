8953f9b60acc0bb32bf7e458558d599dacecad7c
RobinShannon <shannon.rj@googlemail.com>
2024-01-11 15:31:42 +0000

    MechanismGeneration reintoduction first pass

--- src/mechanism_generation/mol_types/species.py
+++ src/mechanism_generation/mol_types/species.py
@@ -475,7 +475,7 @@ class species:
                     hmol = read("H" + str(i) + '_' + str(j) + ".log", index=0)
                     ene = (hmol.get_potential_energy() - baseline) / (invcm)
                 if i > 1 and (ene - ene_arr_1D[-1])  > (smooth*(ene_arr_1D[-1]-ene_arr_1D[-2])):
-                    if (ene_arr_1D[-1]-ene_arr_1D[-2]) == 0 and (ene - ene_arr_1D[-1]) < 0.2 :
+                    if (ene_arr_1D[-1]-ene_arr_1D[-2]) == 0 and (ene - ene_arr_1D[-1]) > 0.2 :
                         pass
                     else:
                         ene = ene_arr_1D[-1]
