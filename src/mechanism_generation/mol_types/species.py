7433f14c4699c5d6b3048a69cadf01dc0cdfde65
RobinShannon <shannon.rj@googlemail.com>
2024-06-24 17:04:09 +0100

    Trying again

--- src/mechanism_generation/mol_types/species.py
+++ src/mechanism_generation/mol_types/species.py
@@ -422,7 +422,7 @@ class species:
                         hmol.set_dihedral(rotatable_bonds[2][0], rotatable_bonds[2][1], rotatable_bonds[2][2],
                                           rotatable_bonds[2][3], diheds[2], indices=coId[2])
                         if ts:
-                            hmol._calc.minimise_ts_write(dihedral=rotatable_bonds,fixed_bonds=bonds_to_fix, path="MultiHind",
+                            hmol._calc.minimise_ts_write(dihedral=rotatable_bonds,fixed_bonds=None, path="MultiHind",
                                                      title="H" + str(i) + '_' + str(j) + '_' + str(l), atoms=hmol,
                                                      rigid=rigid)
                         else:
