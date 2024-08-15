95add6c71a7e1c45cb0d4522d16bb03684d3e340
RobinShannon <shannon.rj@googlemail.com>
2024-08-15 11:48:04 +0100

    Trying again

--- src/Calculators/GaussianCalculator.py
+++ src/Calculators/GaussianCalculator.py
@@ -396,7 +396,7 @@ class Gaussian(FileIOCalculator):
             string += 'B ' + str(bonds[0][0] + 1) + " " + str(bonds[0][1] + 1) +  " F\n"
 
         string += '\n--Link1--'
-        string += '\n%Chk='+str(title)
+        string += '\n%Chk='
         string += '\n%NoSave\n# M062X/6-31+G** Geom=Check Guess=Read opt=(ts, calcall,noeigentest)\n\n'
         string += 'Title\n\n0 2\n'
 
